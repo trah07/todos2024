57db6ef73339af7162396700a1521db2b3087f0e
trah07 <trah07@vse.cz>
2024-04-15 21:24:25 +0200

    Domácí úkol č.7

--- src/websockets.js
+++ src/websockets.js
@@ -1,36 +1,75 @@
-import { WebSocketServer } from 'ws'
-import ejs from 'ejs'
-import { db, getAllTodos } from './db.js'
+import { WebSocketServer } from 'ws';
+import ejs from 'ejs';
+import { db, getAllTodos, getTodoById } from './db.js';
 
-const connections = new Set()
+const connections = new Set();
 
 export const createWebSocketServer = (server) => {
-  const wss = new WebSocketServer({ server })
+  const wss = new WebSocketServer({ server });
 
   wss.on('connection', (socket) => {
-    connections.add(socket)
+    connections.add(socket);
 
-    console.log('New connection', connections.size)
+    console.log('New connection', connections.size);
 
     socket.on('close', () => {
-      connections.delete(socket)
+      connections.delete(socket);
 
-      console.log('Closed connection', connections.size)
-    })
-  })
-}
+      console.log('Closed connection', connections.size);
+    });
+  });
+};
 
-export const sendTodoListToAllConnections = async () => {
-  const todoList = await ejs.renderFile('views/_todos.ejs', {
-    todos: await getAllTodos(),
-  })
+export const sendTodoListToAllConnections = async (operation, todoId) => {
+  if (operation == 'delete') {
+    for (const connection of connections) {
+      connection.send(
+        JSON.stringify({
+          type: 'todoList',
+          todoId: todoId,
+          html: await ejs.renderFile('views/_todos.ejs', {
+            todos: await getAllTodos(),
+          }),
+          operation: operation,
+        })
+      );
+    }
+  } else {
+    const todo = await getTodoById(todoId);
+    for (const connection of connections) {
+      connection.send(
+        JSON.stringify({
+          type: 'todoList',
+          todoId: todo == null ? null : todo.id,
+          html: await ejs.renderFile('views/_todos.ejs', {
+            todos: await getAllTodos(),
+          }),
+          detailHtml: todo == null ? null : await ejs.renderFile('views/todo.ejs', { todo }),
+          operation: operation
+        })
+      );
+    }
+  }    
+};
+
+export const sendTodoDetailToAllConnections = async (todoId) => {
+  const todo = await getTodoById(todoId);
 
   for (const connection of connections) {
     connection.send(
       JSON.stringify({
-        type: 'todoList',
-        html: todoList,
+        type: 'todoDetail',
+        todoId: todo.id,
+        html: await ejs.renderFile('views/_todos.ejs', {
+          todos: await getAllTodos(),
+        }),
+        detailHtml: await ejs.renderFile('views/todo.ejs', { todo }),
+        operation: 'update'
       })
-    )
+    );
   }
-}
+};
+
+export const sendUpdatedTodoDetail = async (todoId) => {
+  await sendTodoDetailToAllConnections(todoId);
+};
